0a5b8803f22b6d1aaf9077fd0db461e60ed6c978
Mateus4222 <mateus13.aa@hotmail.com>
2017-11-21 00:30:28 -0300

    V0.3
    Update Feito
    
    NADA FOI TESTADO AINDA, pois falta integrar com o HTML

--- PHP/database.php
+++ PHP/database.php
@@ -3,10 +3,55 @@
     // Executa as Querys
     function DBExecute($query){
         $link = DBConnect();
-        $result = @mysqli_query($link, $query) or die(mysqli_error());
+        $result = @mysqli_query($link, $query) or die(mysqli_error($link));
         
         DBClose($link);
         return $result;
+    }
+
+    // Alterar Registros
+    function DBUpdate($table, array $data, $where = null){
+        foreach ($data as $key => value){
+            $fields[] = "{$key} = '{$value}'";
+        }
+        
+        $fields = implode (', ', $fields);
+        
+        $where = ($where) ? "WHERE {$where}" : null;
+        
+        $query = "UPDATE {$table} SET {$fields}{$where}";
+        
+        return DBExecute($query);
+    }
+
+    
+    //FUNÇÃO PODERÁ SER USADA PARA O UPDATE
+    // Ler registros
+    function DBRead($table, $params = null, $fields = '*'){
+        $params = ($params) ? "{$params}" : null;
+        
+        $query = "SELECT {$fields} FROM {$table}{$params}";
+        $result = DBExecute($query);
+        
+        if(!mysqli_num_rows($result)){
+            return false;
+        }else{
+            foreach($data as $key => $value){
+                echo "<table>
+                    <tr>
+                        <th>
+                            $key
+                            </th>
+                        </tr>
+                        <tr>
+                        <td>
+                            $values
+                            </td>
+                        </tr>
+                
+                </table>";
+            }
+        }
     }
 
 ?>
