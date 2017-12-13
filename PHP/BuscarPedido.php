6f683c0daed08a5e17e83b53d31c2df170bbd7cc
Mateus4222 <mateus13.aa@hotmail.com>
2017-12-13 03:36:39 -0300

    V.1.2.3
    -Ver pedido funcionando

--- PHP/BuscarPedido.php
+++ PHP/BuscarPedido.php
@@ -7,21 +7,15 @@
     $cod_pedido = $_GET['codigo'];
 
     //Mostrar o pedido a partir do código
-   $teste = DBRead('t_pedido', null, 'nome_pedido, valor_pedido, cod_cliente, cod_pedido');
+   $teste = DBRead('t_pedido', " WHERE cod_pedido = '$cod_pedido'", '*');
    $table= "t_pedido";
         
         echo "<h2>Pedido: </h2><br><br>";
-        $tam = count($teste);
-        if($tam>=$cod_pedido){
             foreach($teste as $key){
-                if($key['cod_pedido'] == $cod_pedido){
+
                      echo "ID: ".$key['cod_pedido'].'<br>';
                      echo "Nome do pedido: ".$key['nome_pedido'].'<br>';
                      echo "Valor do pedido: ".$key['valor_pedido'].'<br>';
                 } 
-            }
-        }else {
-            echo 'Código incompatível';
-        }
    DBClose($link);
 ?>
